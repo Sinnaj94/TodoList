990fa5e76fc193658287d5427d4128e9e3aa8540
Jannis Jahr <>
2018-07-10 13:27:19 +0200

    Added database with sqlite

--- data.py
+++ data.py
@@ -1,2 +1,44 @@
-# Database with mysql
 import sqlite3
+
+conn = sqlite3.connect("data.db")
+
+
+def init():
+    # Create Table for Tasks
+    # TODO: Date has to be checked still...
+    conn.execute("CREATE TABLE IF NOT EXISTS task ("
+                 "task_id INTEGER PRIMARY KEY ASC,"
+                 "timestamp DATETIME DEFAULT CURRENT_DATE NOT NULL, done INT DEFAULT 0 NOT NULL,"
+                 "important INT DEFAULT 0 NOT NULL,"
+                 "due_date DATE NOT NULL CHECK (due_date=STRFTIME('%d-%m-%Y', due_date)),"
+                 "description TEXT NOT NULL, category_id INTEGER NULL)")
+    # Create Table for Category
+    conn.execute("CREATE TABLE IF NOT EXISTS "
+                 "category (category_id INTEGER PRIMARY KEY ASC,"
+                 "category_name TEXT NOT NULL)")
+
+
+def add_task(description, due_date, important=0):
+    conn.execute("INSERT INTO task (description, due_date, important) VALUES (?, ?, ?)", [description, due_date, important])
+    conn.commit()
+
+
+def remove_task(task_id):
+    conn.execute("DELETE FROM task WHERE task_id=?", [task_id])
+    conn.commit()
+
+
+def remove_category(category_id):
+    # TODO: Also remove ID in Task
+    conn.execute("DELETE FROM category WHERE category_id=?", [category_id])
+    conn.commit()
+
+
+def add_category(name):
+    conn.execute("INSERT INTO category (category_name) VALUES(?)", [name])
+    conn.commit()
+
+
+init()
+
+# add_task("Fegen", "02.02.2018")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import request
+from flask import request, g
 from flask import render_template
-
+import data
+# TODO: Implement getter and setter, connect with data.py
 app = Flask(__name__)
 
 
@@ -9,14 +10,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/categories', methods=['GET', 'POST'])
 def categories():
     return 'TODO'
 
-@app.route('/tasks')
+
+@app.route('/tasks', methods=['GET', 'POST'])
 def tasks():
     return render_template('tasks.html', tasks = ['a', 'b', 'c'])
-
 
 
 if __name__ == '__main__':
